da81613b0f38410f574216a3d47a799c1b80fcc3
pascalmad <pascaldinh@icloud.com>
2025-01-06 22:56:10 +0100

    rabbitmq and docker compose init

---
+++ frontend/src/app/sudoku/page.tsx
@@ -0,0 +1,7 @@
+export default function SudokuPage() {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-4xl font-bold">Solve a Sudoku</h1>
+    </div>
+  );
+}
